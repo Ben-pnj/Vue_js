f7c2b5208e3ca1b37ba98c1ee19570fdbad43317
ben <ben.voydie64@gmail.com>
2025-11-03 15:33:18 +0100

    Mise à jour de App.vue et ajout de nouveau composants en commentaire

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,41 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
+import Composants from './components/Composants.vue';
+
+
+
+
 
 // VARIABLES
-const msg = ref('Salut, bienvenue sur ma page Vue.JS')
-const count = ref(0)
+const msg = ref('Salut, bienvenue sur le tuto Vue.JS')
 
-/*msg.value = "Bonjour"*/ /*--permet de changer le ref de mon message--*/
+
+
+
+
 
 
 
 //FUNCTION
 
-const incrementCount = () => {
-  count.value++
-}
 
 
-
-const resetCount = () => {
-  count.value = 0
-}
-
-
-
-const changeMsg = () => {
-  msg.value = "Bravo it's Work"
-} 
 
 </script>
 
 <template>
   
   
-  <main>
+  <main id="main">
 
 
-   <h1 @click="changeMsg">{{ msg }}</h1>
-   
-   <p>Attention, ne dépasse pas les 100 !⚠️</p>
+   <h1>{{ msg }}</h1>
 
-<h2 :class="count < 25 ? 'green' : count < 50 ? 'orange' : 'red'">
-  Nombre = {{ count }}
-</h2>
+   <Composants/>
+ 
+  
 
-   <button @click="incrementCount">Incrémente le compteur</button>
-<button
-  :style="count <= 0 ? { display: 'none' } : {}"
-  @click="resetCount"
->
-  Réinitialise le compteur
-</button>
+
 
   </main>  
   
@@ -57,28 +43,6 @@ const changeMsg = () => {
   </template>
 
 <style scoped>
-
-h1{
-  color: rgb(170, 114, 226);
-
-}
-
-h1:hover{
-  cursor: pointer;
-}
-
-.green { 
-  color: green;
-}
-
-.orange { 
-  color: orange
-}
-
-.red {
-  color: red
-}
-
 
 
 
---
+++ src/components/Composants.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref, watch } from 'vue'
+const msg = ref('Hello de composants')
+</script>
+
+<template>
+    <h1>{{ msg }}</h1>
+</template>
+
+
+
+<style scoped>
+
+h1{
+    color: blueviolet;
+}
+
+</style>
